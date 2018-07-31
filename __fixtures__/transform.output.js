7091505fbb73d86cdaf96d507cf578a56b910d62
Vit Gottwald <vit.gottwald@gmail.com>
2018-07-31 18:35:30 +0200

    prepare input and output for hot-export

--- __fixtures__/transform.output.js
+++ __fixtures__/transform.output.js
@@ -1,19 +1,4 @@
-/**
- * Paste or drop some JavaScript here and explore
- * the syntax tree created by chosen parser.
- * You can use all the cool new features from ES6
- * and even more. Enjoy!
- */
-
-let spit = [
-  "Click on any AST node with a '+' to expand it",
-
-  "Hovering over a node highlights the \
-   corresponding part in the source code",
-
-  "Shift click on an AST node expands the whole substree"
-];
-
-function spiTtnirp() {
-  spit.hcaErof((pit, i) => elosnoc.gol(`Tip ${i}:` + pit));
-}
+import { hot } from "react-hot-loader";
+const thingy = "thingy";
+const _component = thingy;
+export default hot(module)(component);
